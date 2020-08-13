60ca5eaa56203e0b5839ed0716fcaae67589feb2
holmanRinconGit <holman.rincon.git@gmail.com>
2020-08-12 23:29:07 -0500

    JWT cambio de keys

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const app = express();
+var cors = require('cors');
 const { config } = require('./config');
 const usersApi = require('./routes/users');
 const cancellationsApi = require('./routes/cancellations');
 
+app.use(cors());
+
+app.use('/static', express.static('files'));
+
 // body parser
 app.use(express.json());
+
+app.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 //routes
 usersApi(app);
