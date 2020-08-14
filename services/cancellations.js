2cc7a8924fc2cb2f6e42abd9f9dfaf800e8c4494
holmanRinconGit <holman.rincon.git@gmail.com>
2020-08-14 16:31:51 -0500

    Queries actualizadas

--- services/cancellations.js
+++ services/cancellations.js
@@ -4,7 +4,40 @@ class CancellationsService {
   createCancellationOrder({ order }) {
     return new Promise((resolve, reject) => {
       mysqlLib.query(
-        `INSERT INTO CANCELLATIONS (ID_USER, ID_SUBJECT,STATE) VALUES (${order.ID_USER},${order.ID_SUBJECT},'C')`,
+        `INSERT INTO CANCELLATIONS (ID_USER, ID_SUBJECT,STATE,FILE_NAME) VALUES (${order.ID_USER},${order.ID_SUBJECT},'C','${order.FILE_NAME}')`,
+        (err, res) => {
+          if (err) reject(err);
+          else resolve(res);
+        }
+      );
+    });
+  }
+  getCancellations() {
+    return new Promise((resolve, reject) => {
+      mysqlLib.query(
+        `SELECT CANCELLATIONS.ID,USERNAME,EMAIL,NAME,CAREER,STATE,FILE_NAME FROM CANCELLATIONS INNER JOIN USERS ON ID_USER = USERS.ID INNER JOIN SUBJECTS ON ID_SUBJECT = SUBJECTS.ID`,
+        (err, res) => {
+          if (err) reject(err);
+          else resolve(res);
+        }
+      );
+    });
+  }
+  getCancellation({ id }) {
+    return new Promise((resolve, reject) => {
+      mysqlLib.query(
+        `SELECT CANCELLATIONS.ID,USERNAME,EMAIL,NAME,CAREER,STATE,FILE_NAME FROM CANCELLATIONS INNER JOIN USERS ON ID_USER = USERS.ID INNER JOIN SUBJECTS ON ID_SUBJECT = SUBJECTS.ID WHERE CANCELLATIONS.ID = ${id}`,
+        (err, res) => {
+          if (err) reject(err);
+          else resolve(res);
+        }
+      );
+    });
+  }
+  updateCancellation({ id, state }) {
+    return new Promise((resolve, reject) => {
+      mysqlLib.query(
+        `UPDATE CANCELLATIONS SET STATE='${state}' WHERE ID=${id}`,
         (err, res) => {
           if (err) reject(err);
           else resolve(res);
